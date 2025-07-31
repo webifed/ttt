a9a23dfee4af44eb5a7fd1a1aac0b47ca7becefa
Ivan Kostic <113978421+ivan-eagleray@users.noreply.github.com>
2025-07-31 20:33:39 +0200

    added player turns and game over

--- src/Board.js
+++ src/Board.js
@@ -9,14 +9,32 @@ function Square({value, onSquareClick}) {
 
 export default function Board() {
   const [square, setSquare] = useState(Array(9).fill(null));
+  const [playerTurn, setPlayerTurn] = useState("X");
+  let status;
 
   const handleSquareClick = (i) => {
-  let newSquare = [...square];
-  newSquare[i] = 'X';
-  setSquare(newSquare);
-}
+    let newSquare = [...square];
+
+    if (newSquare[i] || calculateWinner(square)) {
+      return;
+    }
+
+    playerTurn === "X" ? setPlayerTurn("Y") : setPlayerTurn("X")
+    newSquare[i] = playerTurn;
+    setSquare(newSquare);
+  }
+
+  const winner = calculateWinner(square)
+
+  if (winner) {
+    status = `GAME OVER! ${winner} wins!`; 
+  } else {
+    status = `Next player: ${playerTurn}`;
+  }
 
   return(
+    <>
+    <div>{status}</div>
     <div className="board">
       <div className="board-row">
         <Square value={square[0]} onSquareClick={()=>handleSquareClick(0)}/>
@@ -34,5 +52,26 @@ export default function Board() {
         <Square value={square[8]} onSquareClick={()=>handleSquareClick(8)}/>
       </div>
     </div>
+    </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
